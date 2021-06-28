476e5f18ef8229a2b22b49abc10f464d06b1d07f
tantiayuvraj9 <84831082+tantiayuvraj9@users.noreply.github.com>
2021-06-28 10:14:12 +0800

    Add files via upload

---
+++ Project 37/js/Quiz.js
@@ -0,0 +1,67 @@
+class Quiz {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function (data) {
+      gameState = data.val();
+    });
+  }
+
+  update(state) {
+    database.ref("/").update({
+      gameState: state,
+    });
+  }
+
+  async start() {
+    if (gameState === 0) {
+      contestant = new Contestant();
+      var contestantCountRef = await database
+        .ref("contestantCount")
+        .once("value");
+      if (contestantCountRef.exists()) {
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question();
+      question.display();
+    }
+  }
+
+  play() {
+    question.hide();
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Result of the Quiz!", 340, 50);
+    Contestant.getPlayerInfo();
+    if (allContestants !== undefined) {
+      debugger;
+      var display_Answers = 230;
+      fill("Blue");
+      textSize(20);
+      text(
+        "**Note: Contestants who answered correctly are highlighted in green color!",
+        130,
+        230
+      );
+      for (var plr in allContestants) {
+        debugger;
+        var correctAnswer = "2";
+        if (correctAnswer === allContestants[plr].answer) {
+          fill("Green");
+        } else {
+          fill("Red");
+        }
+        display_Answers += 30;
+        textSize(20);
+        text(
+          allContestants[plr].name + ": " + allContestants[plr].answer,
+          250,
+          display_Answers
+        );
+      }
+    }
+  }
+}
